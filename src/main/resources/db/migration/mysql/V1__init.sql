1ab3a939270dd6cc1758827ad3c6aacb9233658e
Robson Rocha <robson.proxycode@gmail.com>
2018-02-17 22:01:59 -0200

    alterando o arquivo sql inicial script correto

---
+++ src/main/resources/db/migration/mysql/V1__init.sql
@@ -0,0 +1,49 @@
+DROP TABLE IF EXISTS `empresa`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `empresa` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `cnpj` varchar(255) NOT NULL,
+  `data_atualizacao` datetime NOT NULL,
+  `data_criacao` datetime NOT NULL,
+  `razao_social` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `funcionario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `funcionario` (
+  `id` bigint(20) NOT NULL,
+  `cpf` varchar(255) NOT NULL,
+  `data_atualizacao` date NOT NULL,
+  `data_criacao` date NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `perfil` varchar(255) NOT NULL,
+  `qtd_horas_almoco` float DEFAULT NULL,
+  `qtd_horas_trabalho_dia` float DEFAULT NULL,
+  `senha` varchar(255) NOT NULL,
+  `valor_hora` decimal(19,2) DEFAULT NULL,
+  `empresa_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`,`empresa_id`),
+  KEY `fk_funcionario_empresa_idx` (`empresa_id`),
+  CONSTRAINT `fk_funcionario_empresa` FOREIGN KEY (`empresa_id`) REFERENCES `empresa` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `lancamento`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `lancamento` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `data_atualizacao` datetime NOT NULL,
+  `data_criacao` datetime NOT NULL,
+  `descricao` varchar(255) DEFAULT NULL,
+  `localizacao` varchar(255) DEFAULT NULL,
+  `tipo` varchar(255) DEFAULT NULL,
+  `funcionario_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`,`funcionario_id`),
+  KEY `fk_lancamento_funcionario1_idx` (`funcionario_id`),
+  CONSTRAINT `fk_lancamento_funcionario1` FOREIGN KEY (`funcionario_id`) REFERENCES `funcionario` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
